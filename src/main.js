84efb965bd1bb3fee39af49e1c50aea5d8b18a8c
delphix <delphix2012@gmail.com>
2021-05-21 12:08:22 +0100

    Setup project and create home page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/css/styles.css'
+
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(VueQuillEditor)
 
 Vue.config.productionTip = false
 
